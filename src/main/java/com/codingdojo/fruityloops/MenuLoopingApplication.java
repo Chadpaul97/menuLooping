c4be290464b61c36268f2d5b925de867dcf33f54
Chadpaul97 <chadpaul97@gmail.com>
2021-12-05 18:34:47 -0600

    Fruity_Loops First Commit

---
+++ src/main/java/com/codingdojo/fruityloops/MenuLoopingApplication.java
@@ -0,0 +1,14 @@
+package com.codingdojo.fruityloops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuLoopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuLoopingApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/codingdojo/Daikichi/MenuLoopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Daikichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuLoopingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
